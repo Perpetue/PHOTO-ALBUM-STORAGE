c41f5563fa4c70a1c8c849b467fc13a44b475d19
Queen-Uwitonze <uwitonzeq@gmail.com>
2019-01-16 05:31:41 -0500

    upload images using javascript

--- js/script1.js
+++ js/script1.js
@@ -1,28 +1,3 @@
-// var imageArray =[
-//     "image/images(1)",
-//     "image/images(2)",
-//     "image/images(3)",
-//     "image/images(4)",
-//     "image/images(5)"
-//     ];
-    
-//         function assignUrl(img_num)
-//         {
-//             return "http://lokeshdhakar.com/projects/lightbox2/img/demopage/" + imageArray[img_num];
-//         }
-        
-//         $(function() {
-//             for (var i = 0; i < imageArray.length; i++){
-//                 var div = $("<a/>").attr("href",assignUrl(i)).attr("data-lightbox","imageGallery");
-//                 div.append($("<img/>").attr("src",assignUrl(i)).attr("width","20%").attr("height","20%"));
-//                 console.log($);
-//                 $(".gallery").append(div);    
-//             }
-//             $('a[rel="lightbox"]').lightBox();
-//         });
-
-
-
 
 $("input").change(function(e) {
 
@@ -35,8 +10,9 @@ $("input").change(function(e) {
         reader.onloadend = function() {
              img.src = reader.result;
         }
-        reader.readAsDataURL(file);
-        $("input").after(img);
+        reader.readAsDataURL(this.files[0]);
+        $("input").after(img);//display the preview images
     }
 });
+
 
